91106361fadb01a4ccd5e2d74c0ead3ee303ed70
ritik_rajbhar <ritikrajbhar321@gmail.com>
2024-05-28 16:59:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(90deg, #F87516 0%, #5E11FF 100%)",
+      },
+      fontFamily: {
+        Sora: ["Sora", "sans-serif"],
+        Outfit: ["Outfit", "sans-serif"],
+      },
+      screens: {
+        "extra-sm": "500px",
+        "mid-large": "800px",
+      }
+    },
+  },
+  plugins: [],
+};
